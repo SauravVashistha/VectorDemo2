292f10a1591e5fba70b2213483aec3345a0a936e
Saurav <kamleshhh5013@gmail.com>
2026-03-31 18:59:43 +0530

    first commit

---
+++ src/org/tnsif/cap/c2tc/collections/Vector2.java
@@ -0,0 +1,45 @@
+package org.tnsif.cap.c2tc.collections;
+
+
+
+
+
+
+import java.util.List;
+import java.util.Vector;
+
+public class Vector2 {
+
+	public static void main(String[] args) throws InterruptedException {
+	   List<Integer> numbers=new Vector<>();
+	   
+	   //Thread 1 : adds elements to the vector
+	   Thread thread1=new Thread( ()->{
+	   for(int i=1;i<5;i++)
+	   {
+		   numbers.add(i); //1 2 3 4 
+		   System.out.println("Thread 1 added " + i);
+	   }
+	   });   
+	   
+	   //Thread 2:remove elements from the vector
+	   Thread thread2=new Thread( ()->{
+	   for(int i=1;i<5;i++)
+	   {
+		  if(!numbers.isEmpty())
+		  {
+			  System.out.println("Thread 2 removed " + numbers.remove(0));
+		  }
+	   }
+	});
+
+	   
+	   thread1.start();
+	   thread2.start();
+	   
+	   thread1.join();
+	   thread2.join();
+	   
+	   System.out.println("Numbers left in the vector "+ numbers);
+}
+}
